5d8f85d91bc21bb389f6b0d966c2def3e016684a
Xyehtz <alejitogarbo@gmail.com>
2024-03-10 00:45:45 -0500

    Added loop to the program

--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,32 @@ use rand::distributions::Alphanumeric;
 
 fn main() {
     println!("Welcome to ASCII Creator by Xyehtz");
-    println!("Please enter the name of the image you want to convert: ");
 
-    let mut raw_path: String = String::new();
-    io::stdin()
-        .read_line(&mut raw_path)
-        .expect("Error obtaining path");
-    let path: &str = raw_path.as_str().trim();
+    loop {
+        println!("Please enter the name of the image you want to convert (type \"quit\" to exit): ");
 
-    let result = pixels_to_ascii(img_to_grayscale(resize_img(path)));
+        let mut raw_path: String = String::new();
+        io::stdin()
+            .read_line(&mut raw_path)
+            .expect("Error obtaining path");
+        let path: &str = raw_path.as_str().trim();
 
-    let current_dir = env::current_dir();
-    let current_dir: String = current_dir.unwrap().display().to_string();
+        if path.to_lowercase() == "quit" {
+            break;
+        }
 
-    println!("Exited process with: {:?}\n\n", result.0);
-    println!("Output file: {}\\{}\n\n", current_dir, result.1);
+        let result = pixels_to_ascii(img_to_grayscale(resize_img(path)));
 
-    io::stdin()
-        .read_line(&mut raw_path)
-        .expect("Error closing app");
+        let current_dir = env::current_dir();
+        let current_dir: String = current_dir.unwrap().display().to_string();
+
+        println!("Exited process with: {:?}\n\n", result.0);
+        println!("Output file: {}\\{}\n\n", current_dir, result.1);
+    }
 }
 
 fn resize_img(original_img_path: &str) -> String {
-    println!("Please choose one of the following options for the quality of the image:\n1. Very high\n2. High\n3. Medium\n4. Low\n5. Very low");
+    println!("Please choose one of the following options for the quality of the image (Medium or Low quality is highly recommended):\n1. Very high\n2. High\n3. Medium\n4. Low\n5. Very low");
     let mut option_str: String = String::new();
     io::stdin()
         .read_line(&mut option_str)
@@ -75,7 +78,9 @@ fn pixels_to_ascii(img_path: String) -> (io::Result<()>, String) {
     let binding: String = img_path.replace(&img_path[img_path.len() - 3..img_path.len()], "txt");
     let file_path: &str = binding.as_str();
 
-    let ascii_chars: [char; 39] = ['#', '@', '%', '&', '*', '$', '.', ',', ':', ';', '-', '_', '/', '|', '(', ')', '[', ']', '{', '}', '<', '>', '^', '~', '=', '+', '?', '!', '¬', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    let ascii_chars: [char; 79] = [
+    ' ', '.', ',', '-', '_', '`', '^', '´', '\"', ':', '*', '!', '?', '¡', '¿', '(', ')', '[', ']', '{', '}', '|', '\\', '/', '>', '<', '=', '@', '#', '$', '%', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '\\', '|', '/', '>', '<', '?', '¡', '¿', '~', '`', '^', '\"', ':', ';', ',', '.', '-', '_', '=', '+', '*', '&', '%', '$', '#', '@', '!', '?', '¿', '(', ')', ' ', ' ', ' ', ' ', ' '];
+
     let img: DynamicImage = image::open(&img_path).expect("Error opening the image");
 
     let file: File = File::create(&file_path).expect("Error creating the file");
